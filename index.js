65e5ec61e6badc0ddf53bfb338bf7bd4acfa9033
LerichO <lerichyaso@gmail.com>
2023-12-18 16:48:36 -0600

    finished challenge 7

--- index.js
+++ index.js
@@ -177,7 +177,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [firstGame, secondGame] = sortedGames; 
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topGameName = document.createElement("p");
+topGameName.innerHTML = firstGame.name;
+firstGameContainer.appendChild(topGameName);
 
 // do the same for the runner up item
+const runnerUpGameName = document.createElement("p");
+runnerUpGameName.innerHTML = secondGame.name;
+secondGameContainer.appendChild(runnerUpGameName);
